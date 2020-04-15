f4c87718f186b217ffca78cd817a5769a129ffbe
GoodLort <63586981+GoodLort@users.noreply.github.com>
2020-04-15 20:22:03 +0300

    Add files via upload

---
+++ YaProgramist.bat
@@ -0,0 +1,22 @@
+echo off
+pause
+md Trufanov
+cd Trufanov
+md Luka
+md Pavlovich
+pause
+cd Luka
+echo > 18091998.txt
+cd ..
+cd Pavlovich
+echo > tkMiNeVAuditorii.txt
+cd ..
+cd ..
+pause
+del Trufanov /S /Q /F
+pause
+cd Trufanov
+rd Luka
+rd Pavlovich
+cd ..
+rd Trufanov
